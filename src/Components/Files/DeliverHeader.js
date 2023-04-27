e5c2ea3a23588d376ed3b74427f006e718b965a7
Mhatons <mhatons@gmail.com>
2023-04-27 13:45:21 +0100

    changed the home slider sect

--- src/Components/Files/DeliverHeader.js
+++ src/Components/Files/DeliverHeader.js
@@ -1,6 +1,9 @@
+import bannerImg from '../images/webiste9-09.png'
 const DeliveryHeader = () => {
     return (
-      <div className="bg-light">Hello</div>  
+      <div className="bg_deliver">
+        <img src={bannerImg} alt='Banner Image' />
+      </div>  
     );
 }
  
